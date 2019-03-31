b398da5617ccd914f7509d05260eb1662b580853
maikal kumar <kumarmaikal11@gmail.com>
2019-03-31 12:35:17 +0530

    first commit

---
+++ src/main/java/com/test/edu/TechwhizApplication.java
@@ -0,0 +1,13 @@
+package com.test.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechwhizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechwhizApplication.class, args);
+	}
+
+}
